25fc13e5d16777aea647e638e92d93e64a0e42e2
ax_abror_m3 <abror.mukhammadiev@axtechnology.uz>
2024-03-22 01:39:22 +0500

    feat: main page layout and style

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
 
+  openContactDialog(){}
 }
