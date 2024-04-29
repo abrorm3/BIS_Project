1181e58361585e76250620078c00a9c97e9d9f8c
ax_abror_m3 <abror.mukhammadiev@axtechnology.uz>
2024-04-29 07:19:29 +0500

    feat: minor changes

--- frontend/src/app/booth-details/footer-info/footer-info.component.ts
+++ frontend/src/app/booth-details/footer-info/footer-info.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { CallFormComponent } from '../../call-form/call-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer-info',
   standalone: true,
-  imports: [MatButtonModule],
   templateUrl: './footer-info.component.html',
   styleUrl: './footer-info.component.scss',
+  imports: [MatButtonModule, CallFormComponent, CommonModule],
 })
 export class FooterInfoComponent {
   @Input() price: number | null = null;
+  isContactOpened = signal(false);
+
+  openContactDialog() {
+    this.isContactOpened.set(true);
+    console.log('trr');
+  }
+  closeForm() {
+    this.isContactOpened.set(false);
+  }
 }
